ad9de62eee93cc6a53c9b81ca4533b8f255b91c1
ssamarin <semenssamarin@mail.ru>
2023-03-13 17:51:02 +1000

    fix: description formatting, setting ... if length > 200

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -22,9 +22,12 @@ class MarvelService {
     }
 
     _transformCharacter = (char) => {
+
+        const shortDescr = char.description.length > 150 ? (char.description.slice(0, 200) + '...') : char.description;
+
         return {
             name: char.name,
-            description: char.description,
+            description: shortDescr,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
