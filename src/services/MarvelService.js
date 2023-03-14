1dedca751c142dcf8bbd1dbf2c99d2f9c1e3f76e
ssamarin <semenssamarin@mail.ru>
2023-03-14 19:44:52 +1000

    feat: add Charinfo component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,12 +25,15 @@ class MarvelService {
 
         let shortDescr = char.description.length > 150 ? (char.description.slice(0, 200) + '...') : char.description;
 
+
         return {
+            id: char.id,
             name: char.name,
             description: shortDescr,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
