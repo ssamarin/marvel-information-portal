34a170f7080ac4763aafebf505ffb2b6903d964e
ssamarin <semenssamarin@mail.ru>
2023-03-15 20:55:36 +1000

    feat: add character pagination onClick to load more btn

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=ee4ca9e2431ffab17cba85539fc37296';
+    _baseOffset = 210;
 
     getResources = async (url) => {
         let res = await fetch(url);
@@ -11,8 +12,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
