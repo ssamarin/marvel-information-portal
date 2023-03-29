5ca0527ceedbd5daf68c37477c82d2151e4aee49
ssamarin <semenssamarin@mail.ru>
2023-03-29 22:26:14 +1000

    feat: add ComicsList and pagination comics from server

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,33 +7,69 @@ const useMarvelService = () => {
     const _apiKey = 'apikey=ee4ca9e2431ffab17cba85539fc37296';
     const _baseOffset = 210;
 
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformCharacter);
+	};
 
-    const getAllCharacters = async (offset = _baseOffset) => {
-        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
-        return res.data.results.map(_transformCharacter);
-    }
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+		return _transformCharacter(res.data.results[0]);
+	};
 
-    const getCharacter = async (id) => {
-        const res = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
-        return _transformCharacter(res.data.results[0]);
-    }
+	const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformComics);
+	};
 
-    const _transformCharacter = (char) => {
+	const getComics = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+		return _transformComics(res.data.results[0]);
+	};
 
-        let shortDescr = char.description.length > 150 ? (char.description.slice(0, 200) + '...') : char.description;
+	const _transformCharacter = (char) => {
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description
+				? `${char.description.slice(0, 210)}...`
+				: "There is no description for this character",
+			thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
 
+	const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
 
-        return {
-            id: char.id,
-            name: char.name,
-            description: shortDescr,
-            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage: char.urls[0].url,
-            wiki: char.urls[1].url,
-            comics: char.comics.items
-        }
-    }
-    return {loading, error, getAllCharacters, getCharacter, clearError}
-}
+	return {
+		loading,
+		error,
+		clearError,
+		getAllCharacters,
+		getCharacter,
+		getAllComics,
+		getComics,
+	};
+};
 
 export default useMarvelService;
