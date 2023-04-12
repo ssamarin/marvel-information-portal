83e9c5bf9eeb50f32e5c6e2eb3004d8cf9da15a6
ssamarin <semenssamarin@mail.ru>
2023-04-12 21:44:43 +1000

    feat(Components):add CharSearchForm

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,16 +1,21 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp();
+	const { loading, request, error, clearError } = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _apiKey = 'apikey=ee4ca9e2431ffab17cba85539fc37296';
-    const _baseOffset = 210;
+	const _baseOffset = 210;
 
 	const getAllCharacters = async (offset = _baseOffset) => {
 		const res = await request(
 			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
 		);
+		return res.data.results.map(_transformCharacter);
+	};
+
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
 		return res.data.results.map(_transformCharacter);
 	};
 
@@ -26,7 +31,7 @@ const useMarvelService = () => {
 		return res.data.results.map(_transformComics);
 	};
 
-	const getComics = async (id) => {
+	const getComic = async (id) => {
 		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
 		return _transformComics(res.data.results[0]);
 	};
@@ -66,9 +71,10 @@ const useMarvelService = () => {
 		error,
 		clearError,
 		getAllCharacters,
+		getCharacterByName,
 		getCharacter,
 		getAllComics,
-		getComics,
+		getComic,
 	};
 };
 
